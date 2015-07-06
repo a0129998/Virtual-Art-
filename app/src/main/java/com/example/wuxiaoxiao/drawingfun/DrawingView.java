4b812a8072fa6557d76a78403fc327d305b589b4
a0129998 <a0129998@u.nus.edu>
2015-07-06 17:48:34 +0800

    finished following second part of tutorial

--- app/src/main/java/com/example/wuxiaoxiao/drawingfun/DrawingView.java
+++ app/src/main/java/com/example/wuxiaoxiao/drawingfun/DrawingView.java
@@ -1,18 +1,85 @@
 package com.example.wuxiaoxiao.drawingfun;
 
-import android.view.View;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
 
 
 public class DrawingView extends View {
+
+    private Path drawPath;
+    private Paint drawPaint, canvasPaint;
+    private int paintColor = 0xff6600;
+    private Canvas drawCanvas;
+    private Bitmap canvasBitmap;
+
     public DrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
         setUpDrawing();
     }
 
     public void setUpDrawing(){
+        drawPath = new Path();
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
 
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        canvasPaint = new Paint(Paint.DITHER_FLAG);//paint.dither_flag is a android.graphics
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh){
+        //view given size
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN://touch the view
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE://drag the finger
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP://lift the finger
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+
+        invalidate();//causes onDraw to execute
+        return true;
+    }
+
+    public void setColor(String newColor){
+        invalidate();
+        paintColor = Color.parseColor(newColor);//parseColor turns a string code to color
+        drawPaint.setColor(paintColor);
     }
 
 }
--- app/src/main/java/com/example/wuxiaoxiao/drawingfun/MainActivity.java
+++ app/src/main/java/com/example/wuxiaoxiao/drawingfun/MainActivity.java
@@ -1,17 +1,29 @@
 package com.example.wuxiaoxiao.drawingfun;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private DrawingView drawingView;
+    private ImageButton currPaint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawingView = (DrawingView)findViewById(R.id.drawing);
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+        currPaint = (ImageButton)paintLayout.getChildAt(0);
+        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+
     }
 
     @Override
@@ -34,5 +46,17 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void paintClicked(View view){
+        if(view != currPaint){// view is the button clicked, first check that it is not already selected
+            ImageButton imgView = (ImageButton)view;
+            String color = view.getTag().toString();
+            drawingView.setColor(color);
+
+            imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+            currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
+            currPaint=(ImageButton)view;
+        }
     }
 }
