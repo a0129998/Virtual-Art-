6a5d7bfce798931e6695d7acda9d467065ab3483
a0129998 <a0129998@u.nus.edu>
2015-07-06 16:43:39 +0800

    followed first tutorial
    the background of drawingView does not show properly on my pc :/

---
+++ app/src/main/java/com/example/wuxiaoxiao/drawingfun/DrawingView.java
@@ -0,0 +1,18 @@
+package com.example.wuxiaoxiao.drawingfun;
+
+import android.view.View;
+import android.content.Context;
+import android.util.AttributeSet;
+
+
+public class DrawingView extends View {
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setUpDrawing();
+    }
+
+    public void setUpDrawing(){
+
+    }
+
+}
